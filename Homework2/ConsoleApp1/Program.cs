177adf84f1f06f2d8ca804d6482dc9329cba1b63
Pavel-Levanok <105275978+Pavel-Levanok@users.noreply.github.com>
2022-07-15 16:51:02 +0300

    Homework2 completed

---
+++ Homework2/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number");
+            int a = int.Parse(Console.ReadLine());
+            int f = 1;
+            for (int i = 1; i <= a; i++)
+            {
+            f = f * i;
+            }
+            Console.WriteLine("Factorial is ",f); Console.WriteLine(f);
+        }
+    }
+}
